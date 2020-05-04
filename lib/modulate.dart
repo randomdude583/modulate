daa638ffcf586daac2f8a4c298378d6eee36546d
randomdude583 <randomdude@wetpaintdesigns.com>
2020-05-04 14:52:12 -0500

    fixed #1

--- lib/modulate.dart
+++ lib/modulate.dart
@@ -1,7 +1,88 @@
 library modulate;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:math';
+
+import 'package:baseconvert/baseconvert.dart';
+
+
+
+
+modulate({
+  String input,
+  int base,
+  int width,
+  bool reverse=false,
+}){
+
+  //Find how many binary bytes can fit into a baseX byte
+  int factor = -1;
+  while(true){
+    if(pow(base, width) >= pow(256, factor+1)){
+      factor+=1;
+    } else {
+      if(factor == 0){
+        throw Exception("BAD BASE/WIDTH");
+      }
+      break;
+    }
+  }
+
+
+  //Convert
+  if(!reverse){
+    BaseConverter bc = BaseConverter(2, base, string: true);
+    String output = "";
+    for(int i=0; i<input.length; i+= 8*factor){
+      int endIndex = i+(8*factor);
+      if(endIndex > input.length) endIndex = input.length;
+      String outSeg = bc.convert(input.substring(i, endIndex));
+      outSeg = outSeg.padLeft(width, "0");
+      if(outSeg.length > width){
+        throw Exception("BAD BASE/WIDTH");
+      }
+      output = output + outSeg;
+    }
+    return output;
+
+  } else {
+    BaseConverter bc = BaseConverter(base, 2, string: true);
+    String output = "";
+    for(int i=0; i<input.length; i+= width){
+      int endIndex = i + width;
+      if(endIndex > input.length) endIndex = input.length;
+      String outSeg = bc.convert(input.substring(i, endIndex));
+      if(outSeg.length % (8*factor) != 0){
+        outSeg = outSeg.padLeft(outSeg.length + (8-(outSeg.length % 8)), "0");
+      }
+      output = output + outSeg;
+    }
+    return output;
+
+  }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO implement
+//Take in either string or filename
+//return bytes only when requested.
+//return as string or as stream
+class ModulateStream {
+
 }
--- test/modulate_test.dart
+++ test/modulate_test.dart
@@ -1,13 +1,80 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:modulate/modulate.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+
+  //"Test" in each base encoding, 2-10
+  String B2 = "01010100011001010111001101110100";
+  String B3 = "010010010202011021011022";
+  String B4 = "1110121113031310";
+  String B5 = "11424101421211";
+  String B6 = "03143142430000000312";
+  String B7 = "150203223224";
+  String B8 = "2506256300000164";
+  String B9 = "1136008000000138";
+  String B10 = "2160529556";
+
+
+  group('modulate_StringInOut', ()
+  {
+    test('modulate_Base3_StringInOut', () {
+      expect(modulate(input: B2, base: 3, width: 6), B3);
+    });
+    test('modulate_Base4_StringInOut', () {
+      expect(modulate(input: B2, base: 4, width: 4), B4);
+    });
+    test('modulate_Base5_StringInOut', () {
+      expect(modulate(input: B2, base: 5, width: 7), B5);
+    });
+    test('modulate_Base6_StringInOut', () {
+      expect(modulate(input: B2, base: 6, width: 10), B6);
+    });
+    test('modulate_Base7_StringInOut', () {
+      expect(modulate(input: B2, base: 7, width: 3), B7);
+    });
+    test('modulate_Base8_StringInOut', () {
+      expect(modulate(input: B2, base: 8, width: 8), B8);
+    });
+    test('modulate_Base9_StringInOut', () {
+      expect(modulate(input: B2, base: 9, width: 8), B9);
+    });
+    test('modulate_Base10_StringInOut', () {
+      expect(modulate(input: B2, base: 10, width: 5), B10);
+    });
   });
+
+
+  group('modulate_StringInOut_reverse', ()
+  {
+    test('modulate_Base3_StringInOut', () {
+      expect(modulate(input: B3, base: 3, width: 6, reverse: true), B2);
+    });
+    test('modulate_Base4_StringInOut', () {
+      expect(modulate(input: B4, base: 4, width: 4, reverse: true), B2);
+    });
+    test('modulate_Base5_StringInOut', () {
+      expect(modulate(input: B5, base: 5, width: 7, reverse: true), B2);
+    });
+    test('modulate_Base6_StringInOut', () {
+      expect(modulate(input: B6, base: 6, width: 10, reverse: true), B2);
+    });
+    test('modulate_Base7_StringInOut', () {
+      expect(modulate(input: B7, base: 7, width: 3, reverse: true), B2);
+    });
+    test('modulate_Base8_StringInOut', () {
+      expect(modulate(input: B8, base: 8, width: 8, reverse: true), B2);
+    });
+    test('modulate_Base9_StringInOut', () {
+      expect(modulate(input: B9, base: 9, width: 8, reverse: true), B2);
+    });
+    test('modulate_Base10_StringInOut', () {
+      expect(modulate(input: B10, base: 10, width: 5, reverse: true), B2);
+    });
+  });
+
+
+
+
+
 }
